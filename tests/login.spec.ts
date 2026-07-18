b7f1ce9396a9059c750caafe710c3470eb495122
RenatoBonettiNeto <bonettineto.renato03@gmail.com>
2026-07-18 13:52:15 -0300

    feat: Adicionado testes para registro e login do colaborador.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,19 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
-import { validRecruiter } from "../fixtures/user";
+import { validCollaborator, validRecruiter } from "../fixtures/user";
 
 test.describe("Login Recruiter", () => {
   test("Deve permitir que o recrutador faça login com sucesso", async ({ page }) => {
     const loginPage = new LoginPage(page);
 
     await loginPage.login(validRecruiter);
+  });
+});
+
+test.describe("Login Collaborator", () => {
+  test("Deve permitir que o colaborador faça login com sucesso", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.login(validCollaborator);
   });
 });
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,11 +1,19 @@
 import { test, expect } from "@playwright/test";
 import { RegisterPage } from "../pages/RegisterPage";
-import { validRecruiter } from "../fixtures/user";
+import { validCollaborator, validRecruiter } from "../fixtures/user";
 
 test.describe("Register Recruiter", () => {
   test("Deve cadastrar um recrutador com sucesso", async ({ page }) => {
     const registerPage = new RegisterPage(page);
 
     await registerPage.register(validRecruiter);
+  });
+});
+
+test.describe("Register Collaborator", () => {
+  test("Deve cadastrar um coalborador com sucesso", async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+
+    await registerPage.register(validCollaborator);
   });
 });
