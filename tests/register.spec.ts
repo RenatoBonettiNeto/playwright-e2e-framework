83a692966fca0208ed4f94c2840df1d134777f00
RenatoBonettiNeto <bonettineto.renato03@gmail.com>
2026-07-20 22:58:46 -0300

    feat: Realizado adaptações mo registro de usuário.

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,19 +1,27 @@
 import { test, expect } from "@playwright/test";
 import { RegisterPage } from "../pages/RegisterPage";
-import { validCollaborator, validRecruiter } from "../fixtures/user";
+import { createCollaborator, createRecruiter } from "../fixtures/user";
+import { UserApi } from "../helpers/api/userApi";
 
 test.describe("Register Recruiter", () => {
-  test("Deve cadastrar um recrutador com sucesso", async ({ page }) => {
+  test("Deve cadastrar um recrutador com sucesso", async ({
+    page,
+    request,
+  }) => {
+    const recruiter = createRecruiter();
+
     const registerPage = new RegisterPage(page);
 
-    await registerPage.register(validRecruiter);
+    await registerPage.register(recruiter);
   });
 });
 
 test.describe("Register Collaborator", () => {
   test("Deve cadastrar um coalborador com sucesso", async ({ page }) => {
+    const collaborator = createCollaborator();
+
     const registerPage = new RegisterPage(page);
 
-    await registerPage.register(validCollaborator);
+    await registerPage.register(collaborator);
   });
 });
